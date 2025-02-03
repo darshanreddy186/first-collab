c5b090cebe9a2a847fd075c3d89127857c11ef22
S v Akshay Kumar <152531143+SvAkshayKumar@users.noreply.github.com>
2025-02-03 16:10:07 +0530

    Update Subtract.py
    I have changed the code.

--- Subtract.py
+++ Subtract.py
@@ -1,10 +1,6 @@
-
-def subtract_numbers(num1, num2):
-    return num1 - num2
-
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-result = subtract_numbers(num1, num2)
+result = num1 - num2
 
 print(f"The result of {num1} - {num2} is: {result}")
